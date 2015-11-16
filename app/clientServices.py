8a2a0ab669836774e2b6c06c12150404e7d757fb
Andrew McClain <andrewmcclain@email.arizona.edu>
2015-11-15 18:04:23 -0700

    indentation fixes

--- app/clientServices.py
+++ app/clientServices.py
@@ -16,19 +16,18 @@ def searchSpecific():
     payload = {'location': location, 'radius': 5000, 'types': "food|restaurant", 'query': genre, 'key': placesApiKey}
     r = requests.post(textSearch, params=payload, headers=headers);
     return Response(r.text, content_type='application/json')
-    
+
 @client.route('/client/nearbySuggested', methods=['POST'])
-     
-       
+
+
 @client.route('/client/nearbyAny', methods=['POST'])
 def searchNearby():
     data_dict = request.get_json()
 
     latitude = data_dict['latitude']
     longitude = data_dict['longitude']
-    location = latitude + "," + longitude 
+    location = latitude + "," + longitude
     payload = {'location': location, 'radius': 5000, 'types': 'food', 'key': placesApiKey}
-
     r = requests.post(nearbySearch, params=payload, headers=headers)
     return Response(r.text, content_type="application/json")
 
--- app/repository.py
+++ app/repository.py
@@ -8,12 +8,12 @@ from passlib.hash import md5_crypt
 def createUser(username, password, email):
 	db = get_db()
 	encryptedPass = md5_crypt.encrypt(password)
-    db.execute("INSERT into Users (username, password, email) VALUES (?, ?, ?)", [username, encryptedPass, email])
-    db.commit()
-    cur = db.execute("SELECT userId, username, password, email FROM Users WHERE username = (?)", [email])
-    db.commit()
-    entries = [dict(userID=row[0], username=row[1], password=row[2], email=row[3]) for row in cur.fetchall()]
-    return jsonify(data=entries)
+	db.execute("INSERT into Users (username, password, email) VALUES (?, ?, ?)", [username, encryptedPass, email])
+	db.commit()
+	cur = db.execute("SELECT userId, username, password, email FROM Users WHERE username = (?)", [email])
+	db.commit()
+	entries = [dict(userID=row[0], username=row[1], password=row[2], email=row[3]) for row in cur.fetchall()]
+	return jsonify(data=entries)
 
 def updateUser(userId, username, password, email):
 	db = get_db()
@@ -27,10 +27,10 @@ def updateUser(userId, username, password, email):
 
 	
 def get_db():
-    if not hasattr(g, 'db'):
-        g.db = connect_db()
-        g.db.row_factory = sqlite3.Row
-    return g.db
+	if not hasattr(g, 'db'):
+		g.db = connect_db()
+		g.db.row_factory = sqlite3.Row
+	return g.db
     
 def connect_db():
-    return sqlite3.connect('/var/www/lunchroll/app/database/lunchroll.db')
+	return sqlite3.connect('/var/www/lunchroll/app/database/lunchroll.db')
