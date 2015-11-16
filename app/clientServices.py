b69efbfaddcb3352e98eb488efde27f10491c56a
Andrew McClain <andrewmcclain@email.arizona.edu>
2015-11-15 18:21:57 -0700

    missing random code ???

--- app/clientServices.py
+++ app/clientServices.py
@@ -1,9 +1,10 @@
 from app import app
 import repository
-from flask import Response, jsonify
+from flask import Blueprint, Response, jsonify
 import requests
 
 #Start Client Calls
+client = Blueprint('client', __name__)
 
 @client.route('/client/nearbySpecific', methods=['POST'])
 def searchSpecific():
@@ -14,7 +15,7 @@ def searchSpecific():
     genre = data_dict['genre']
     genre = genre + '+food'
     payload = {'location': location, 'radius': 5000, 'types': "food|restaurant", 'query': genre, 'key': placesApiKey}
-    r = requests.post(textSearch, params=payload, headers=headers);
+    r = requests.post(textSearch, params=payload, headers=headers)
     return Response(r.text, content_type='application/json')
 
 @client.route('/client/nearbySuggested', methods=['POST'])
