58f984bc2c3e5525649430042839619871368a97
Andrew McClain <andrewmcclain@email.arizona.edu>
2015-11-16 01:32:37 -0700

    indentation error

--- app/repository.py
+++ app/repository.py
@@ -43,8 +43,8 @@ def make_dicts(cursor, row):
 	return dict((cursor.description[idx][0], value) for idx, value in enumerate(row))
 
 def query_db(query, args=(), one=False):
-    cur = get_db().execute(query, args)
-    rv = cur.fetchall()
+	cur = get_db().execute(query, args)
+	rv = cur.fetchall()
 	cur.commit()
-    cur.close()
-    return (rv[0] if rv else None) if one else rv
+	cur.close()
+	return (rv[0] if rv else None) if one else rv
