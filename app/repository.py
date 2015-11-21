e1ced19897b5f893363a1255f042cb62cfee2a32
Andrew McClain <andrewmcclain@email.arizona.edu>
2015-11-21 13:18:10 -0700

    small type error

--- app/repository.py
+++ app/repository.py
@@ -122,13 +122,13 @@ def deleteGroup(groupId, password):
 
 def get_all_users():
     cur = query_db("SELECT * FROM Users")
-    entries = [dict(userId=row[0], username=row[1], password=row[2], email=row[3]) for row in cur.fetchall()]
+    entries = [dict(userId=row[0], username=row[1], password=row[2], email=row[3]) for row in cur]
     return entries
 
 
 def get_all_groups():
     cur = query_db("SELECT * FROM Groups")
-    entries = [dict(groupId=row[0], userId=row[1], name=row[2], users=row[3]) for row in cur.fetchall()]
+    entries = [dict(groupId=row[0], userId=row[1], name=row[2], users=row[3]) for row in cur]
     return entries
 
 
