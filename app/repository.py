3a78d1655e86381e108e1885bd3224c1441c3892
Andrew McClain <andrewmcclain@email.arizona.edu>
2015-11-22 23:22:27 -0700

    last fix on getFriends, this time for sure
    zis time fo shou

--- app/repository.py
+++ app/repository.py
@@ -140,7 +140,7 @@ def remove_friend(userId, friend_userId):
 
 def get_user_friends(userId):
     cur = query_db("SELECT friendId FROM Friends WHERE UserId=(?)", [userId], one=False)
-    operationReport = [dict(friendId=row[1]) for row in cur]
+    operationReport = [dict(friendId=row[0]) for row in cur]
     return operationReport
 
 
